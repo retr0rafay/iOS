9c35860931acb16de25624a507fcddb1dbc1493c
Rafay Syed <32440307+retr0rafay@users.noreply.github.com>
2024-04-04 11:31:23 -0400

    Add files via upload

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Rafay Syed on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
